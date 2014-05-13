dc5ab373af259e05d82aee14f93a6e3210a46a93
daemontus <daemontus@gmail.com>
2014-05-13 09:15:07 +0200

    Basic main for launching application.

---
+++ UserInterface/src/fi/muni/pv168/ui/Main.java
@@ -0,0 +1,12 @@
+package fi.muni.pv168.ui;
+
+public class Main {
+
+    //How to start DB: java -jar /Applications/NetBeans/glassfish-4.0/javadb/lib/derbyrun.jar server start
+
+
+    public static void main(String[] args) {
+        new HomeScreen();
+    }
+
+}
